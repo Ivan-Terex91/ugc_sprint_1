06608564fca5c391c9a036301d5d20515b1b6ac2
Asman Malikov <asmanmalikov@gmail.com>
2021-07-08 21:27:40 +0300

    добавил авто создание таблиц в clickhouse

---
+++ docker-entrypoint-initdb.d/init_clickhouse_node3-shard.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE shard;
+CREATE TABLE shard.views (user_id String, movie_id String, viewing_progress Int64, viewing_date DateTime) Engine=ReplicatedMergeTree('/clickhouse/tables/shard2/views', 'replica_1') PARTITION BY toYYYYMMDD(viewing_date) ORDER BY user_id;
